36444f7fa9435c6c05ecdc7d8aa3ef9f0d71384c
Andy.w <andy@gmail.com>
2019-03-27 10:00:30 -0300

    Update demod.sh
    Cosmetic changes to the help screen

--- demod.sh
+++ demod.sh
@@ -1,13 +1,31 @@
 #!/bin/bash
 
 function show_help () {
+        # This function simply shows the script usage.
+        #
         echo "DeMod v.2.1 - written by Andy Vasile <andy.vasile@hostpapa.com>"
         echo
-        echo "Usage: bash <(curl -s http://andyvasile.net/s/demod.sh) -[d|e|l] <DOMAIN.TLD> <RULE-ID>"
+        echo "Usage: bash <(curl -s http://andyvasile.net/s/demod.sh) -[options] domain.tld rule-id"
         echo
-        echo "d: Deactivate | e: Enable | l: List"
+        echo "Options:"
+        echo "-d . Disable"
+        echo "-e . Enable"
+        echo "-l . List (please add a dummy number at the end)"
         echo
         exit 0
+}
+
+function restart_apache(){
+  # This function is a prototype. Right now it checks apache syntax and ask the user to restart it manually.
+  # It will be updated later to restart apache automatially when Syntax is OK.
+  #
+  httpd -t
+  #
+  echo
+  echo "Please restart Apache if 'Syntax OK' was shown above"
+  #
+  echo "/scripts/restartsrv_httpd"
+  echo
 }
 
 if [[ ! $# -eq 3 ]]; then
@@ -15,7 +33,6 @@ if [[ ! $# -eq 3 ]]; then
         # if no parameters, will print the usage & finish the script
         #
         show_help
-        echo "1"
 else
         #
         # if parameter was added it will use that one
@@ -28,7 +45,7 @@ else
                 -d) flag=xa; shift 1;;
                 -e) flag=xd; shift 1;;
                 -l) flag=xl; shift 1;;
-                -*) show_help; echo "2"; break;;
+                -*) show_help; break;;
                 *) break;;
               esac
             done
@@ -55,10 +72,4 @@ servername=$(/var/cpanel/cwaf/scripts/cwaf-cli.pl -l | grep "^$targetDomain")
 #----------------------------------
 /var/cpanel/cwaf/scripts/cwaf-cli.pl -d "$servername" -$flag "$rule"
 #
-httpd -t
-#
-echo
-echo "Please restart Apache if 'Syntax OK' was shown above"
-#
-echo "/scripts/restartsrv_httpd"
-echo
+restart_apache
